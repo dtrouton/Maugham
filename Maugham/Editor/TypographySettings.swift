398f9dd9c32710e7e5a496a0aafb7d90070186d3
Denver Trouton <dtrouton@gmail.com>
2026-05-07 13:19:33 +0100

    refactor: add TypographySettings placeholder for WritingMode compilation
    This is a temporary placeholder that will be fully implemented in Task 5.
    Added to unblock the build and allow Theme tests to run.

---
+++ Maugham/Editor/TypographySettings.swift
@@ -0,0 +1,7 @@
+import Foundation
+
+/// Typography settings for applying to editor text.
+/// Full implementation in Task 5.
+public struct TypographySettings: Equatable, Sendable {
+    // Placeholder for now
+}
