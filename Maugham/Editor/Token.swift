8010fc95c1b0a1f555fd06ab49f14e72157f243e
Denver Trouton <dtrouton@gmail.com>
2026-05-07 13:15:59 +0100

    feat: add Token type for syntax highlighting

---
+++ Maugham/Editor/Token.swift
@@ -0,0 +1,25 @@
+import Foundation
+
+/// A classified range of source text, produced by a `WritingMode`'s tokenizer
+/// and consumed by the editor coordinator to apply theme attributes.
+public struct Token: Equatable, Sendable {
+    public enum Kind: Equatable, Sendable {
+        case heading(level: Int)
+        case emphasis(strong: Bool)
+        case code
+        case link(href: String)
+        case listMarker
+        case blockquote
+        case horizontalRule
+        case syntaxPunctuation
+        case plain
+    }
+
+    public let range: NSRange
+    public let kind: Kind
+
+    public init(range: NSRange, kind: Kind) {
+        self.range = range
+        self.kind = kind
+    }
+}
---
+++ MaughamTests/TokenTests.swift
@@ -0,0 +1,27 @@
+import XCTest
+@testable import Maugham
+
+final class TokenTests: XCTestCase {
+    func test_kindEquality_distinguishesHeadingLevels() {
+        XCTAssertNotEqual(Token.Kind.heading(level: 1), .heading(level: 2))
+        XCTAssertEqual(Token.Kind.heading(level: 3), .heading(level: 3))
+    }
+
+    func test_kindEquality_distinguishesEmphasisStrength() {
+        XCTAssertNotEqual(Token.Kind.emphasis(strong: false),
+                          .emphasis(strong: true))
+    }
+
+    func test_kindEquality_distinguishesLinkHrefs() {
+        XCTAssertNotEqual(Token.Kind.link(href: "a"),
+                          .link(href: "b"))
+        XCTAssertEqual(Token.Kind.link(href: "a"),
+                       .link(href: "a"))
+    }
+
+    func test_token_equatableByRangeAndKind() {
+        let a = Token(range: NSRange(location: 0, length: 5), kind: .plain)
+        let b = Token(range: NSRange(location: 0, length: 5), kind: .plain)
+        XCTAssertEqual(a, b)
+    }
+}
